f6731472759fc40029f1690f9f10ed194e6dc430
Sriman Win <Sriman Win@DESKTOP-65CS97E>
2020-08-03 00:28:30 +0800

    Specflow changes

---
+++ Features/AddProfile.feature
@@ -0,0 +1,44 @@
+﻿Feature: SellerProfilePage
+ As a Seller
+ I want to add my profile details
+
+ @mytag
+Scenario: Add Description
+	Given login to Mars application as a seller
+    And navigate to profile page
+    When click on the edit button of Description
+    Then should be able to Add description
+    Examples: 
+    | description               |
+    | This is dummy description |
+     
+  @Negative test case
+  Scenario: Add blank in Description field
+      Given loging to Mars Application as a seller
+      And navigate to profile page
+      When click on the edit button of discription
+      And seller should try to add empty valu in description field
+      Then seller should not able to add empty valu in discription field
+      Examples: 
+    | description|
+    ||
+   
+  @Positive test case
+  Scenario: Add language In Language field
+    Given as a seller loging to Mars Application
+    And seller navigate to profile page
+    When seller click on the add language button 
+    Then seller should able to add language
+    Examples: 
+    | language |
+    | English  |
+  
+  @Negative
+  Scenario: Add Duplicate language in language field
+   Given as a seller loging to Mars Application
+   And seller navigate to profile page
+   When seller click on the add language button
+   Then seller should not be able to add language
+   Examples: 
+   | language |
+   | English  |
---
+++ StepDefination/AddProfile.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MarsQA.StepDefination
+{
+    [Binding]
+    public class AddProfile
+    {
+        [Given(@"login to Mars application as a seller")]
+        public void GivenLoginToMarsApplicationAsASeller()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"navigate to profile page")]
+        public void GivenNavigateToProfilePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"click on the edit button of Description")]
+        public void WhenClickOnTheEditButtonOfDescription()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"should be able to Add description")]
+        public void ThenShouldBeAbleToAddDescription()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"loging to Mars Application as a seller")]
+        public void GivenLogingToMarsApplicationAsASeller()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"click on the edit button of discription")]
+        public void WhenClickOnTheEditButtonOfDiscription()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"seller should try to add empty valu in description field")]
+        public void WhenSellerShouldTryToAddEmptyValuInDescriptionField()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"seller should not able to add empty valu in discription field")]
+        public void ThenSellerShouldNotAbleToAddEmptyValuInDiscriptionField()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"as a seller loging to Mars Application")]
+        public void GivenAsASellerLogingToMarsApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"seller navigate to profile page")]
+        public void GivenSellerNavigateToProfilePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"seller click on the add language button")]
+        public void WhenSellerClickOnTheAddLanguageButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"seller should able to add language")]
+        public void ThenSellerShouldAbleToAddLanguage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"seller should not be able to add language")]
+        public void ThenSellerShouldNotBeAbleToAddLanguage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
